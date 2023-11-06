90a870a84ff4b30b50b836cd973b522db690e35a
Hampus Selldén <hampussellden1@gmail.com>
2023-11-06 21:54:23 +0100

    quick fix

--- app/_components/ClientApartmentEditor.tsx
+++ app/_components/ClientApartmentEditor.tsx
@@ -1,5 +1,5 @@
 'use client'
-import React, { use, useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import classNames from 'classnames';
 import { Apartment, KitchenType, Front, Worktop, FrontOption, WorktopOption} from '../types';
 import { createClient } from '@/utils/supabase/client';
