28c96a8bbaf7558b57f32c190e94e33e2123e831
Hampus Selldén <hampussellden1@gmail.com>
2023-11-08 12:49:05 +0100

    update middleware next js routing
    - more restricitons on routes

--- middleware.ts
+++ middleware.ts
@@ -15,6 +15,17 @@ export async function middleware(req: NextRequest) {
   const { data: role, error } = await supabase.from('users').select('app_role').eq('id', user?.id).single();
   const app_role = role?.app_role;
 
+  // if user is not signed in and the current path is not /login redirect the user to /login
+  if (!user && req.nextUrl.pathname !== '/login') {
+    return NextResponse.redirect(new URL('/login', req.url));
+  }
+
+  if (user && session && req.nextUrl.pathname === '/login') {
+    return NextResponse.redirect(new URL('/', req.url));
+  }
+  if (user && session && req.nextUrl.pathname === '/admin' && app_role !== 'admin') {
+    return NextResponse.redirect(new URL('/', req.url));
+  }
   // if user is signed in and the current path is / redirect the user to /dashboard
   if (user && session && req.nextUrl.pathname === '/') {
     if (app_role === 'admin') {
@@ -24,14 +35,9 @@ export async function middleware(req: NextRequest) {
     }
   }
 
-  // if user is not signed in and the current path is not /login redirect the user to /login
-  if (!user && req.nextUrl.pathname !== '/login') {
-    return NextResponse.redirect(new URL('/login', req.url));
-  }
-
   return res;
 }
 
 export const config = {
-  matcher: ['/', '/account'],
+  matcher: ['/', '/account', '/admin', '/dashboard', '/login'],
 };
