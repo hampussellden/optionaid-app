36940a3131d3ab753f656d8696e310b732dc08b8
Hampus Selldén <hampussellden1@gmail.com>
2023-11-24 00:09:10 +0100

    minor margin increase on large screens

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,8 +9,11 @@ export default async function RootLayout({ children }: { children: React.ReactNo
       <head>
         <title>Optionaid</title>
       </head>
-      <body className="bg-background text-foreground ">
-        <main className="min-h-screen flex flex-col items-center mx-10 gap-4 mb-10 relative" id="content">
+      <body className="bg-background text-foreground flex flex-col items-stretch">
+        <main
+          className="min-h-screen flex flex-col items-center mx-16 xl:mx-20 2xl:mx-24 gap-4 mb-10 relative"
+          id="content"
+        >
           <header className="flex flex-row justify-between w-full mt-4 bg-static p-4 rounded">
             <a href="/" className="focus:outline-none">
               <h1 className="text-3xl font-bold ">Optionaid</h1>
